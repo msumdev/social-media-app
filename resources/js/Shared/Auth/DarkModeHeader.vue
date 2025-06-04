d3e8fad3dfa1d98066671291be6a0c7259a6ab32
w16020397 <michaeltheoneandonly69@gmail.com>
2025-06-04 03:05:37 +0100

    gitlab > github

--- resources/js/Shared/Auth/DarkModeHeader.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <button type="button" class="day-night-btn day-night-btn-two mr_10" @click="toggleDayNight">
-        <img src="/images/daylight.png" class="icon24 image-black" alt="daylight">
-        <img src="/images/moon.png" class="icon24 image-black" alt="moon">
-    </button>
-</template>
-<script>
-export default {}
-</script>
